e6a046d99cec73fa7ab104a59c54f3c6ceb28425
nadiaell <nadiaell@hotmail.com>
2025-07-22 13:09:44 -0300

    cree usseefect pata obtencion de las peliculas

--- src/pages/DetallePelicula.jsx.jsx
+++ src/pages/DetallePelicula.jsx.jsx
@@ -1,5 +1,5 @@
-import React,{useEffect,useState} from "react";
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "../api/axios"
 
 
@@ -11,5 +11,27 @@ import {
       CardContex,
       Container,
       Button,
-} from "@mui/material"import { useParams } from "react-router-dom";
+} from "@mui/material"
 
+
+export default function DetallePelicula() {
+      const { id } = useParams()
+      const [pelicula, setPelicula] = useState(null)
+
+      useEffect(() => {
+            const fetchPelicula = async () => {
+                  try {
+                        const res = await axios.get(`movie/${id}`);
+                        setPelicula(res.data)
+                  } catch (error) {
+                        console.log("error cargando detalle de PELICULA", error)
+                  }
+            }
+            fetchPelicula()
+      },[id])
+
+
+
+
+
+};
