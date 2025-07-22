4f26b4ccc01231e7c5fdcd2ba78b2d695fa9f708
nadiaell <nadiaell@hotmail.com>
2025-07-22 13:47:41 -0300

    aplique mui para la card de peli populares

--- src/pages/Populares.jsx
+++ src/pages/Populares.jsx
@@ -18,5 +18,31 @@ useEffect(()=>{
       }
       fetchPopulares()
 },[])
+return(
+      <div>
+      <Typography variant="h4" gutterBottom sx={{ marginTop: '20px' }}>
+        🎬 Películas Populares
+      </Typography>
+      <Grid container spacing={4}>
+        {peliculasPopulares?.map((peli) => (
+          <Grid item xs={12} sm={6} md={4} key={peli.id}>
+            <Link to={`/pelicula/${peli.id}`} style={{ textDecoration: 'none' }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="400"
+                  image={`https://image.tmdb.org/t/p/w500${peli.poster_path}`}
+                  alt={peli.title}
+                />
+                <Paper sx={{ padding: '10px', textAlign: 'center' }}>
+                  <Typography variant="h6">{peli.title}</Typography>
+                </Paper>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+)
 
 }
