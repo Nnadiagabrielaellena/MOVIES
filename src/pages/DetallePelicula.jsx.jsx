00ff6209930ffbc1f4121acaede1f38cb4efd07c
nadiaell <nadiaell@hotmail.com>
2025-07-22 12:59:16 -0300

    importe useparams y useeffect con lA Api key

---
+++ src/pages/DetallePelicula.jsx.jsx
@@ -0,0 +1,15 @@
+import React,{useEffect,useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from "../api/axios"
+
+
+import {
+      Typography,
+      Box,
+      Card,
+      CardMedia,
+      CardContex,
+      Container,
+      Button,
+} from "@mui/material"import { useParams } from "react-router-dom";
+
