173b3d420bc6770ddbd2310aa499dd8743c4695f
nadiaell <nadiaell@hotmail.com>
2025-07-22 10:43:47 -0300

    creacion de rutas dentrto de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./pages/Home";
+import Populares from "./pages/Populares";
+import UltimosLanzamientos from "./pages/UltimosLanzamientos";
+import DetallePelicula from "./pages/DetallePelicula"
+import MejoresPuntuadas from "./pages/MejoresPuntuadas"
+import Favoritos from "./pages/Favoritos";
+import Header from "./components/Header"
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+<Header/>
+<Routes>
+  <Route path='/' element ={<Home/>}/>
+  <Route path='/populares' element={<Populares/>}/>
+  <Route path='ultimos-lanzamientos' element={<UltimosLanzamientos/>}/>
+  <Route path='/mejores-puntuadas' element={<MejoresPuntuadas/>}/>
+  <Route path='/favoritos' element={<Favoritos/>} />
+  <Route path='/peliculas/:id' element={<DetallePelicula/>}/>
+
+
+
+
+</Routes>
+
+
+
+
+
+      </Router>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { MovieProvider } from './context/MovieContext'; 
 import App from './App.jsx'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <MovieProvider>
+     <App />
+    </MovieProvider>
+  </React.StrictMode>
+);
