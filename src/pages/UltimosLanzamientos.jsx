7a704223d179a299ef14da89dcd0542745969a1f
nadiaell <nadiaell@hotmail.com>
2025-07-22 14:18:04 -0300

    aplique useeffect para obtener datos buscados

--- src/pages/UltimosLanzamientos.jsx
+++ src/pages/UltimosLanzamientos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMovies } from "../context/MovieContext";
-import{Typography, Grid, Card,CardMedia,                 </Card>
+import{Typography, Grid, Card,CardMedia,                
 } from "@mui/material"
 import { Link } from "react-router-dom";
 
