b7e6e87ccd3c337ed003a5e72962089516e6fce0
nadiaell <nadiaell@hotmail.com>
2025-07-23 11:46:44 -0300

    agregando . env

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
-    api_key: '7c7eaf93f4bdd728a68f7a22f2138a1a', 
+    api_key: import.meta.env.VITE_API_KEY,
     language: 'es-ES',
   },
 });
